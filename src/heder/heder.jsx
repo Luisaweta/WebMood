b49aee4ec97ea7310c01a362aa5130754e7cea1f
Luisaweta <166143880+Luisaweta@users.noreply.github.com>
2024-05-28 17:53:26 +0300

    реализован переход между страницами

--- src/heder/heder.jsx
+++ src/heder/heder.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "./heder.css"
 export default function Heder(){
     return(
@@ -9,10 +10,10 @@ export default function Heder(){
 
         <div id = "menu">
           <ul >
-            <li><button className="menubutton">Главная</button></li>
-            <li><button className="menubutton">Услуги</button></li>
-            <li><button className="menubutton">О нас</button></li>
-            <li><button className="menubutton">Контакты</button></li>
+            <li><Link to="/"><button className="menubutton">Главная</button></Link></li>
+            <li><Link to="/UslugiPage"><button className="menubutton">Услуги</button></Link></li>
+            <li><Link to="/OnasPage"><button className="menubutton">О нас</button></Link></li>
+            <li><Link to="/KontaktPage"><button className="menubutton">Контакты</button></Link></li>
           </ul>
         </div>
 
@@ -22,9 +23,7 @@ export default function Heder(){
                 <li>+7 918-386-32-24</li>
             </ul>
         </div>
-        
       </header>
-
     )
 
 }
---
+++ src/pages/KontaktPage.jsx
@@ -0,0 +1,13 @@
+import Heder from "../heder/heder"
+import Footer from "../fotter/fotter"
+
+export default function KontaktP(){
+
+    return(
+        <div>
+            <Heder />
+
+            <Footer/>
+        </div>
+    )
+}
